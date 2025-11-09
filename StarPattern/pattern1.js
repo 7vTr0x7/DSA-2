5b613e1f47521d02b11d215b32eca53809a17998
7vTr0x7 <7vtrox7@gmail.com>
2025-11-09 14:51:50 +0530

    Pattern1

---
+++ StarPattern/pattern1.js
@@ -0,0 +1,15 @@
+// *
+// * *
+// * * *
+// * * * *
+
+const patter1 = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < i + 1; j++) {
+      row += " * ";
+    }
+    console.log(row);
+  }
+};
+patter1(5);
